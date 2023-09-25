3452696a6c57095be3a85a61dd2382152e942b11
Nicholas Fantozz <nicholas.fantozz@skechers.com>
2023-09-25 14:19:14 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import * as core from '@actions/core'
+import { github } from '@actions/github';
+
+async function run() {
+  try {
+    const token = core.getInput('token');
+    const repoOwner = github.context.repo.owner;
+    const repo = github.context.repo.repo;
+    const octokit = github.getOctokit(token)
+    const { data: pullRequest } = await octokit.rest.pulls.list({
+      owner: repoOwner,
+      repo: repo,
+      state: 'open',
+      sort: 'long-running',
+    })
+    core.setOutput('open-pr-list', pullRequest);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+run();
